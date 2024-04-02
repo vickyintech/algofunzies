7e0112946eb192069aad2f727e16072bd3db6afc
Victoria Mei <contactvickymei@gmail.com>
2024-04-02 16:39:35 -0400

    provide another solution using recursion

--- 1071GreatestCommonDivisor.js
+++ 1071GreatestCommonDivisor.js
@@ -2,6 +2,7 @@
 // Leetcode https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
 
 function gcdOfStrings (string1, string2) {
+
  if(str1 + str2 !== str2 + str1 ) {return ''}
     
     let m = str1.length, k = str2.length
@@ -16,3 +17,29 @@ function findGCD (length1, length2) {
     if(length2 === 0) return length1
     return findGCD(length2, length1%length2)
  }
+
+
+/* Another solution 
+
+CASE 1  string 1 + string 2 = string 2 + string 1
+CASE 2  string1 === string2
+CASE 3  string 1 > string 2 or string 2 > string 1
+
+"ABABAB"  "ABAB"
+"AB" "ABAB" 
+return "AB"
+
+*/
+
+function gcdOfStrings (string1, string2) {
+    if(str1 + str2 !== str2 + str1 ) {
+        return ''
+    } else if (str1 === str2) { 
+        return str1 
+    }
+    else if (str1.length > str2.length) {
+        return gcdOfStrings( str1.substr(str2.length), str2)
+    } else {
+        return gcdOfStrings( str2.substr(str1.length), str1)
+    }
+}
