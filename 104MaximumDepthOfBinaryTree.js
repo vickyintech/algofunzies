348212a84ccf251554e0463ff345bd6dbf20d17c
Victoria Mei <contactvickymei@gmail.com>
2024-04-03 16:31:13 -0400

    add in solution for binary tree

--- 104MaximumDepthOfBinaryTree.js
+++ 104MaximumDepthOfBinaryTree.js
@@ -1,4 +1,4 @@
-// Algo expert tbd
+// Algo expert https://www.algoexpert.io/questions/node-depths
 // Leetcode https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
 //Input: root = [3,9,20,null,null,15,7]
@@ -7,6 +7,13 @@
 // Input: root = [1,null,2]
 // Output: 2
 
+// Use depth first search
+// Time O(n) Space O(n)
+
 function maxDepth (root) {
-    
+    if(root === null) {
+        return 0
+    }else {
+         return 1+ Math.max(maxDepth(root.left), maxDepth(root.right))
+    }
 }
