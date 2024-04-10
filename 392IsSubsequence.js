9ccb2661420c9ae608b0aba423e6b31835004164
Victoria Mei <contactvickymei@gmail.com>
2024-04-10 17:15:16 -0400

    add in solutions for 392

--- 392IsSubsequence.js
+++ 392IsSubsequence.js
@@ -1,5 +1,14 @@
 // Leetcode : https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
 
 function isSubsequence (s, t) {
+    let i = 0 // pointer for s
+    let j = 0 // pointer for t
 
+    while(i< s.length && j< t.length) {
+        if(s[i] === t[j]) { 
+            i++
+        }
+        j++
+    }
+    return i===s.length
 }
