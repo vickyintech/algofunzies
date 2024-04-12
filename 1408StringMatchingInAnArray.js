7240a1d3a01eaf2548adb33fdca3918863db0f48
Victoria Mei <contactvickymei@gmail.com>
2024-04-12 13:28:58 -0400

    add in more algos

---
+++ 1408StringMatchingInAnArray.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/string-matching-in-an-array/
+
+ //words = ["mass","as","hero","superhero"]
+ //Output: ["as","hero"]
+// brute force solutin
+// Time O(N*2) Space O(N)
+
+function stringMatching (words) {
+    let res = new Set()
+        for(let i = 0; i<words.length; i++) {
+            for(let j=0; j<words.length; j++) {
+                console.log(words[i])
+                console.log(words[j])
+                if(words[i] !== words[j] && words[i].includes(words[j])){
+                    res.add(words[j])
+                }
+            }
+        }
+        return Array.from([...res])
+}
+
+function stringMatching (words) {
+    return words.filter(n=> words.some( el=> el !== word && el.includes(word)))
+}
---
+++ 70ClimbingStairs.js
@@ -0,0 +1,5 @@
+
+
+function climbStairs (n) {
+    
+}
