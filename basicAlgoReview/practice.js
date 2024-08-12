9bc8e69aa5403bec6d5fb37fa30e768252f62e74
Victoria Mei <contactvickymei@gmail.com>
2024-08-12 18:20:04 -0400

    create basic algo folder and organized un numbered files

---
+++ basicAlgoReview/practice.js
@@ -0,0 +1,69 @@
+/*
+This file is just a playground for practices as going through basic algos
+*/
+
+//reverse string 
+
+function reverseString(s) { 
+    return s.split('').reduce((acc, el) => el+reversed, '' )
+}
+
+// Palindrome
+// 1. palindrome("abba") === true
+// 2. palindrome("abcdefg") === false
+
+function isPalindrome(s) { 
+    s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    let start = 0, end = s.length - 1
+    while (start < end) { 
+        if (s[start] !== s[end]) return false 
+        start++
+        end --
+    }
+    return true
+}
+
+// Integer reversal  
+/*
+Examples:
+1. reverseInt(15) === 51;
+2. reverseInt(981) === 189;
+3. reverseInt(500) === 5;
+4. reverseInt(-15) === -51;
+5. reverseInt(-90) === -9;
+*/
+
+function reverseInt(n) { 
+    let s = n.toString().split('')
+    let reversed = ''
+    for (let el of s) { 
+        reversed = el + reversed
+    }
+    return Math.sign(n) < 0? parseInt('-'+ reversed) : parseInt(reversed)
+}
+
+// Max chars 
+// 1.  maxChar("abcccccccd") === "c"
+// 2.  maxChar("apple 1231111") === "1"
+
+function maxChar(s) { 
+    
+}
+
+
+// FizzBuzz
+function fizzBuzz(n) { 
+    for (let i = 1; i < n; i++) {
+        let num = n[i]
+        if (num % 3 && num % 5) {
+            console.log('fizzbuzz')
+        } else if (num % 3) {
+            console.log('fizz')
+        } else if (num % 5) {
+            console.log('buzz')
+        } else { 
+            console.log('i')
+        }
+    }
+}
+
