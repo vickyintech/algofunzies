bedb02f70c3c0e5054fcd78f652f3cd17e728f1e
Victoria Mei <contactvickymei@gmail.com>
2024-08-23 16:41:02 -0400

    classic threesum solution

--- 015_threeSum.js
+++ 015_threeSum.js
@@ -1,5 +1,28 @@
 // Leetcode https://leetcode.com/problems/3sum/description/
 
 function threeSum(nums) { 
+    nums.sort((a, b) => a - b)
+    let ans = []
+    for (let i = 0; i < nums.length; i++) { 
+        if (i> 0 && nums[i] === nums[i - 1]) continue // continue means skip the following operation
+        let start = i + 1, end = nums.length - 1
+        while (start < end) { 
+            let sum = nums[i] + nums[start] + nums[end]
+            if (sum > 0) {
+                end--
+            } else if (sum < 0) {
+                start++
+            } else {
+                ans.push([num[i], num[start], num[end]])
+                start++
+                end --
+                while (start < end && nums[start] === nums[start - 1]) { 
+                    start ++
+                }
+            }
+        }     
+    }
+
+    return ans
     
 }
