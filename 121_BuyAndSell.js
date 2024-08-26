ac3ff67bfe33be130691c4ea7d0e4f039b0fbf55
Victoria Mei <contactvickymei@gmail.com>
2024-08-26 17:10:50 -0400

    add solution

--- 121_BuyAndSell.js
+++ 121_BuyAndSell.js
@@ -1,5 +1,16 @@
-// Leetcode: 
+// Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 function maxProfit(prices) { 
-
+    let left = 0, right = 1, maxProfit = 0
+    while (left < prices.length - 1) { 
+        if (prices[left] < prices[right]) {
+            const currentMargin = prices[right] - prices[left]
+            maxProfit = Math.max(currentMargin, maxProfit)
+        } else { 
+            // sliding widow 
+            left = right 
+        }
+        right ++
+    }
+    return maxProfit
 }
