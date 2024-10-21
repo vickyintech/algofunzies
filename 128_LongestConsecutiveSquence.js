4b60ed5bafdb1eddcb566ea944c6e7f28c1ca25b
Victoria Mei <contactvickymei@gmail.com>
2024-10-21 14:31:18 -0400

    add in more algos

--- 128_LongestConsecutiveSquence.js
+++ 128_LongestConsecutiveSquence.js
@@ -4,5 +4,17 @@
 //nums = [100,4,200,1,3,2]
 
 function longest(nums) { 
-    
+    const tracker = new Set(nums)
+    const longest = 0
+
+    for (let i = 0; i < nums.length; i++) { 
+        if (!tracker.has(nums[i] - 1)) { 
+            let length = 1
+            while (tracker.has(num[i] + length)) { 
+                length ++
+            }
+            longest = Math.max(length, longest)
+        }
+    }
+    return longest
 }
