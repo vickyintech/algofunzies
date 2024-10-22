988f6627ea0d2d2be19d07f66ae52dff0e13c24e
Victoria Mei <contactvickymei@gmail.com>
2024-10-22 11:54:45 -0400

    solution for binary search problem 704

--- 704_BinarySearch.js
+++ 704_BinarySearch.js
@@ -1,6 +1,20 @@
 // Leetcode: https://leetcode.com/problems/binary-search/
 
+// This is a fundamental problem and easy as well
+
 function binarySearch(nums, target) { 
     //nums = [-1,0,3,5,9,12], target = 9
-    
+    let left = 0, right = nums.length - 1 
+    while (left <= right) { 
+        const mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) {
+            return mid
+            
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        } else { 
+            left = mid + 1
+        }
+    }
+    return -1 
 }
