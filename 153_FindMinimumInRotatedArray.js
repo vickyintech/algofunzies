4e835f19599a47953ea1c02b08452fd2c3d062c6
Victoria Mei <contactvickymei@gmail.com>
2024-10-23 09:18:01 -0400

    add in solution for 153

--- 153_FindMinimumInRotatedArray.js
+++ 153_FindMinimumInRotatedArray.js
@@ -1,5 +1,20 @@
 // Leetcode: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+// Input: nums = [3,4,5,6,1,2]
+// Output: 1
+
+// When array is sorted and try to search, use binary search
+
 function findMin(nums) { 
-    
+    let left = 0, right = nums.length - 1 
+    while (left < right) { 
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] < nums[right]) {
+            // search left 
+            right = mid
+        } else { 
+            left = mid + 1
+        }
+    }
+    return nums[left]
 }
