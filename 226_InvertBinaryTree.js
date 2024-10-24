987c2bd53aa412bde6bd01661c7925f53e295a2e
Victoria Mei <contactvickymei@gmail.com>
2024-10-24 09:25:57 -0400

    alternative solution to invertBinaryTree

--- 226_InvertBinaryTree.js
+++ 226_InvertBinaryTree.js
@@ -1,5 +1,7 @@
 // Leetcode: https://leetcode.com/problems/invert-binary-tree/description/
 
+// Use recursion with depth first search
+
 function TreeNode(val, left, right) { 
     this.val = (val === undefined ? 0 : val)
     this.left = (left === undefined? null: left)
@@ -12,4 +14,24 @@ function invertTree(root) {
     node.left = this.invertTree(root.right)
     node.right = this.invertTree(root.left)
     return node
+}
+
+// Resolve it with a class
+
+class TreeNode { 
+    constructor(value, left, right)  { 
+        this.value = value
+        this.left = left
+        this.right = right 
+    }
+}
+
+class solution { 
+    invertTree(root) { 
+        if (root === null) return null
+        let node = new TreeNode(root.value)
+        node.left = invertTree(root.right)
+        node.right = invertTree(root.left)
+        return node 
+    }
 }
